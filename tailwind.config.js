1a68b95f012ad692934d91706c9955f31253eb2f
Louis Cuvelier <hello@louiscuvelier.com>
2020-11-07 18:18:17 +0100

    Added responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,27 @@ module.exports = {
   },
   purge: [],
   theme: {
+    screens: {
+      xs: "360px",
+      ...defaultTheme.screens
+    },
     fontFamily: {
       title: ["Aviano", ...defaultTheme.fontFamily.serif],
       subtitle: ["PT Sans", ...defaultTheme.fontFamily.sans],
       body: ["Futura", ...defaultTheme.fontFamily.sans]
     },
     extend: {
+      spacing: {
+        "72": "18rem",
+        "84": "21rem",
+        "96": "24rem"
+      },
       fontSize: {
-        "7xl": "5rem"
+        "7xl": "5rem",
+        "8xl": "6rem"
+      },
+      letterSpacing: {
+        "widest-xl": "0.15em"
       }
     }
   },
